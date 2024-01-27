81d5a932cb5387f04732f938b05aa45d7f778b7d
Suman-A1 <sumanpervaiz3@gmail.com>
2024-01-27 21:06:07 +0500

    add design in admin

--- frontend/src/pages/PayNow.jsx
+++ frontend/src/pages/PayNow.jsx
@@ -5,13 +5,34 @@ const PayNow = () => {
   const { donationInfo } = useSelector((state) => state.donate);
   return (
     <>
-      <div className="mt-5 pt-5">
-        <h1 className="">Pay Now</h1>
-        <p>Type: {donationInfo.type}</p>
-        <p>Org: {donationInfo.org}</p>
-        <p>Amount:{donationInfo.amount}</p>
-        <p>Full Name: {donationInfo.fullName}</p>
-        <p>Email: {donationInfo.email}</p>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="mt-5 pt-5 ms-5">
+              <h3 className="text-success">Complete your donation!</h3>
+              <p className="text-secondary mt-4 fs-5">
+                <span className=" fs-5 text-dark">Type: </span>
+                {donationInfo.type}
+              </p>
+              <p className="text-secondary fs-5">
+                <span className=" fs-5 text-dark"> Organization: </span>
+                {donationInfo.org}
+              </p>
+              <p className="text-secondary fs-5">
+                <span className=" fs-5 text-dark">Donation: </span>
+                {donationInfo.amount}Rs.
+              </p>
+              <p className="text-secondary fs-5">
+                <span className=" fs-5  text-dark">Full Name: </span>
+                {donationInfo.fullName}
+              </p>
+              <p className="text-secondary fs-5">
+                <span className=" fs-5 text-dark">Email: </span>
+                {donationInfo.email}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- frontend/src/pages/auth/Admin.jsx
+++ frontend/src/pages/auth/Admin.jsx
@@ -13,25 +13,59 @@ const Admin = () => {
   };
   return (
     <>
-      <div>
-        <div className="row">
-          <div className="col-1 bg-danger"></div>
-        </div>
-        <Link to="/">
-          <button className="btn btn-primary">Home</button>
-        </Link>
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <h1>{userInfo.name}</h1>
-              <h1>{userInfo.email}</h1>
+      <div className="container d-flex  mt-5 pt-5  ">
+        <div className="row ">
+          <div className="col-md-3 ">
+            <div>
+              <Link to="/">
+                <button className="btn btn-primary px-4 ">Home</button>
+              </Link>
+            </div>
+            <div className="mt-3">
+              <Link to="/">
+                <button className="btn btn-primary px-4">Zakat</button>
+              </Link>
+            </div>
+            <div className="mt-3">
+              <Link to="/">
+                <button className="btn btn-primary px-4">Tithing</button>
+              </Link>
+            </div>
+            <div className="mt-5 pt-5">
+              <Link to="/">
+                <button className="btn btn-primary  "> Donations</button>
+              </Link>
+            </div>
+            <div className="mt-4">
+              <button className="btn btn-danger px-4" onClick={handleLogout}>
+                Logout
+              </button>
             </div>
           </div>
-          <div className="row">
-            <button onClick={handleLogout}>Logout</button>
-          </div>
+          <div className="col-md-9"></div>
         </div>
       </div>
+      {/* <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-danger"></div>
+          </div>
+          <Link to="/">
+            <button className="btn btn-danger">Home</button>
+          </Link>
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h1>{userInfo.name}</h1>
+                <h1>{userInfo.email}</h1>
+              </div>
+            </div>
+            <div className="row">
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          </div>
+        </div>
+      </div> */}
     </>
   );
 };
