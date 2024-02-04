69ca6954adf71b3de20b67bef1261330dba51a7f
Hafiz Faisal Ali <hafizfaisalali605@gmail.com>
2024-02-04 18:21:17 +0500

    App ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import colors from "colors";
 import dotenv from "dotenv";
+import path from "path";
 import errorHandler from "./middlewares/errorHandler.js";
 import ConnectDb from "./config/db.js";
 import authRoutes from "./routes/auth.js";
@@ -20,11 +21,19 @@ app.get("/api", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/donate", donateRouter);
 
+const __dirname = path.resolve();
+if (!process.env.PORT === "development") {
+  app.use("/", express.static(path.join(__dirname, "frontend", "dist")));
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.use(errorHandler);
-const port = process.env.NODE_PORT | 5555;
+const port = process.env.PORT | 5555;
 app.listen(port, (req, res) => {
   console.log(
-    `Server is running on port ${process.env.NODE_PORT} in ${process.env.NODE_MODE} Mode`
+    `Server is running on port ${process.env.PORT} in ${process.env.NODE_MODE} Mode`
       .bgGreen
   );
 });
