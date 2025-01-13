32933c5cb3968989ab268b81d2f3cbacc34174c4
Hafiz Faisal Ali <143261310+HafizFaisalAli@users.noreply.github.com>
2025-01-13 13:45:27 +0500

    Update user.js

--- backend/data/user.js
+++ backend/data/user.js
@@ -1,20 +1,8 @@
 import bcrypt from "bcryptjs";
 const users = [
   {
-    name: "Suman",
-    email: "sumanpervaiz3@gmail.com",
-    password: bcrypt.hashSync("Qwe123@@", 10),
-    isAdmin: true,
-  },
-  {
-    name: "Mehak Riaz",
-    email: "mehakriaz@gmail.com",
-    password: bcrypt.hashSync("Qwe123@@", 10),
-    isAdmin: true,
-  },
-  {
-    name: "Iram",
-    email: "iram@gmail.com",
+    name: "",
+    email: "",
     password: bcrypt.hashSync("Qwe123@@", 10),
     isAdmin: true,
   },
